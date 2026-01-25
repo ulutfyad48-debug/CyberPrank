dd09919867600c5d27fbedee31d6b3104a30ed30
ulutfyad48-debug <ulutfyad48@gmail.com>
2026-01-25 12:55:54 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,35 @@
-const CACHE_NAME = 'cyberprank-v1';
-const urlsToCache = [
-  '/CyberPrank/',
-  '/CyberPrank/index.html',
-  '/CyberPrank/manifest.json',
-  '/CyberPrank/service-worker.js',
-  '/CyberPrank/icons/icon-72.png',
-  '/CyberPrank/icons/icon-96.png',
-  '/CyberPrank/icons/icon-128.png',
-  '/CyberPrank/icons/icon-144.png',
-  '/CyberPrank/icons/icon-152.png',
-  '/CyberPrank/icons/icon-192.png',
-  '/CyberPrank/icons/icon-512.png'
-];
+const CACHE_NAME = "cyberprank-cache-v1";
+const OFFLINE_URL = "./";
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
