bfb2d030020c171db765c9afbb651b6a6a65a320
ulutfyad48-debug <ulutfyad48@gmail.com>
2026-01-25 14:06:25 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "cyberprank-v1";
+
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
