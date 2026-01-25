b9563f46b01b05459d37665b60d6203f5082687d
ulutfyad48-debug <ulutfyad48@gmail.com>
2026-01-25 12:35:34 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-// Service Worker for CyberPrank
-const CACHE_NAME = 'cyberprank-v1.0';
+const CACHE_NAME = 'cyberprank-v1';
 const urlsToCache = [
   '/CyberPrank/',
   '/CyberPrank/index.html',
@@ -10,49 +9,20 @@ const urlsToCache = [
   '/CyberPrank/icons/icon-128.png',
   '/CyberPrank/icons/icon-144.png',
   '/CyberPrank/icons/icon-152.png',
-  '/CyberPrank/icons/icon-180.png',
   '/CyberPrank/icons/icon-192.png',
-  '/CyberPrank/icons/icon-384.png',
   '/CyberPrank/icons/icon-512.png'
 ];
 
-// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache opened');
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
